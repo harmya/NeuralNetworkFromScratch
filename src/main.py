69d4627f817c228c9dcb2d6ffb313bd60f708a28
Harmya Bhatt <60983274+harmya@users.noreply.github.com>
2023-04-02 21:20:00 -0400

    data input

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Load the data
+data_train = pd.read_csv('/Users/harmyabhatt/NeuralNetworkFromScratch/data/mnist_train.csv')
+data_test = pd.read_csv('/Users/harmyabhatt/NeuralNetworkFromScratch/data/mnist_test.csv')
+
+
+
+
