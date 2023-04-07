c3b0c3bae735cb05abc015dc4ebbc1dbe26d60bb
Harmya Bhatt <60983274+harmya@users.noreply.github.com>
2023-04-07 17:20:42 -0400

    fix the test output

--- src/neuralNetwork.py
+++ src/neuralNetwork.py
@@ -6,11 +6,12 @@ import matplotlib.pyplot as plt
 
 # Sigmoid function for activation
 def sigmoid(z):
-        return 1.0/(1.0+np.exp(-z))
+    return 1.0 / (1.0 + np.exp(-z))
+    
 
 # Derivative of sigmoid function
 def sigmoid_derivative(z):
-    return sigmoid(z)*(1-sigmoid(z))
+    return sigmoid(z) * (1.0 - sigmoid(z))
 
 # Derivative of the cost function with respect to the activation of the output layer
 def cost_derivative_wrt_a(output_activations, y):
@@ -73,18 +74,83 @@ class NeuralNetwork(object):
     
     # now we can apply backporpogation to update the weights and biases of the network
     # but the issue is that, for the true cost function, we need to sum over all the training examples
-    # 
+    # that is computaionally expensive, hence we do a stochastic gradient descent
+    # stochastic gradient descent is a method of updating the weights and biases of the network by taking a small step in the direction of the gradient of the cost function
+    # this small step is computed by using the backpropogation algorithm for a mini batch of training examples
+
+    def update_mini_batch(self, mini_batch, learning_rate):
+        nabla_cost_biases = [np.zeros(b.shape) for b in self.biases]
+        nabla_cost_weights = [np.zeros(w.shape) for w in self.weights]
+
+        for x, y in mini_batch:
+            delta_nabla_cost_biases, delta_nabla_cost_weights = self.backpropogation(x, y)
+            nabla_cost_biases = [bias + delta_bias for bias, delta_bias in zip(nabla_cost_biases, delta_nabla_cost_biases)]
+            nabla_cost_weights = [weight + delta_weight for weight, delta_weight in zip(nabla_cost_weights, delta_nabla_cost_weights)]
+
+        self.weights = [w-(learning_rate/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_cost_weights)]
+        self.biases = [b-(learning_rate/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_cost_biases)]
         
+    # now we can train the network by using the update_mini_batch function 
+    # and implementing the stochastic gradient descent algorithm
 
-        
+    def train(self, training_data, epochs, mini_batch_size, learning_rate, test_data=None):
+        if test_data:
+            n_test = len(test_data)
+        n = len(training_data)
+        for epoch in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, learning_rate)
+            if test_data is not None:
+                print("Epoch {0}: {1} / {2}".format(epoch, self.evaluate(test_data), n_test))
+            else:
+                print("Epoch {0} complete".format(epoch))
 
+    # now we can evaluate the network by using the feedforward function
+    # and comparing the output of the network with the actual output
 
-
-
-
-
+    def evaluate(self, test_data):
+        test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
+        for r in test_results:
+            print(r)
+            print("------------------")
+        return sum(int(x == y) for (x, y) in test_results)
 
 # Test the neural network
+
+# load the data
+def load_data():
+    training_data = pd.read_csv("../data/mnist_train.csv")
+    test_data = pd.read_csv("../data/mnist_test.csv")
+
+    training_data = training_data.values
+    test_data = test_data.values
+
+    print(training_data.shape)
+    print(test_data.shape)
+
+    training_data = [(x[1:].reshape(784, 1), vectorized_result(x[0])) for x in training_data]
+    test_data = [(x[1:].reshape(784, 1), vectorized_result(x[0])) for x in test_data]
+    
+
+
+    return (training_data, test_data)
+
+def vectorized_result(j):
+    e = np.zeros((10, 1))
+    e[j] = 1.0
+    return e
+
+
+# run the neural network
+
+training_data, test_data = load_data()
+net = NeuralNetwork([784, 30, 10])
+net.train(training_data, 30, 10, 3.0, test_data=test_data)
+
+# the network is trained and we can now test it
+
 
 
 
