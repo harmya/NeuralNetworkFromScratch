7bab931e716b47e6a3d1b4016468893d29f5b5ea
harmya <hvbhatt@purdue.edu>
2024-04-08 05:17:34 -0400

    imports

--- src/test.py
+++ src/test.py
@@ -5,6 +5,3 @@ from neuralNetwork import NeuralNetwork
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 b = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
-print(a[-1])
-
-# bruh
--- src/transformer.py
+++ src/transformer.py
@@ -1,25 +1,7 @@
-import torch 
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+import torch
 import torch.nn as nn
 import math
-
-
-class InputEmbeddings(nn.Module):
-    def __init__(self, d_model : int, vocab_size: int):
-        super().__init__()
-        self.d_model = d_model
-        self.vocab_size = vocab_size
-        self.embedding = nn.Embedding(vocab_size, d_model)
-    
-    def forward(self, x):
-        return self.embedding(x) * math.sqrt(self.d_model)
-
-class PositionalEncoding(nn.Module):
-    def __init(self, d_model: int, seq_len: int, dropout: float) -> None:
-        super().__init__()
-        self.d_model = d_model
-        self.seq_len = seq_len
-        self.dropout = dropout
-
-        positional_encoding = torch.zeros(d_model, seq_len)
-        position_index = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
-        denominator = torch.exp(torch.arange(0, d_model, 2).float()) 
+import re
